92534206bb57ffa1b8cd1bae3c4063e2c936fa4f
idecous <idecous@gmail.com>
2014-10-14 19:58:06 +0800

    mobile navigation update

--- public_en/js/customer/checkout/cart.js
+++ public_en/js/customer/checkout/cart.js
@@ -2,14 +2,26 @@ var EBE_DeleteManager = function(){
 	var formEl = $(".common_pageContent form:eq(0)");
 	var inputEl = formEl.find("input");
 	
-	var idEls =  $(".common_centerBlock table tbody tr td ");
+	var idEls =  $(".common_centerBlock table tbody tr td[class=hidden] input");
 	var tableDelEls = $(".common_centerBlock table tbody tr td .delBtn"); 
 	var mobileDelEls = $(".common_centerBlock .mobileBlock li .delBtn"); 
 	
+	tableDelEls.click(function(){
+		var tIndex = tableDelEls.index(this);
+		inputEl.val( $.trim(idEls.eq(tIndex).val()) );
+		
+		
+	});
+	mobileDelEls.click(function(){
+		var tIndex = tableDelEls.index(this);
+		inputEl.val( $.trim(idEls.eq(tIndex).val()) );
+		
+		console.log( inputEl );	
+	});
+
 	
 	
 	
-	console.log( idEls );	
 };
 
 
