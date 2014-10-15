d1285a512e4c3fd854a52b17f0604d09f69d5891
idecous <idecous@gmail.com>
2014-10-15 22:10:06 +0800

    favorites update

--- public_en/js/customer/account/favorites.js
+++ public_en/js/customer/account/favorites.js
@@ -2,6 +2,16 @@ var EBE_ModuleGroup = function(){
 	var navBarEls = $(".common_rightPanel .shortcutBar a");
 	var moduleEls = $(".common_rightPanel .moduleGroup .module");
 	
+	moduleEls.each(function(index){
+		var moduleEl = moduleEls.eq(index);
+		var itemEls =  moduleEl.children(".item");
+		var extraCount = 3 - itemEls.length % 3;
+		for(var i=0; i < extraCount;i++){
+			$("<div class='item'></div>").appendTo(moduleEl);
+		}
+        $("<div class='justifyFix'</div>").appendTo(moduleEl);
+	});
+
 	navBarEls.click(function(){
 		var tIndex = navBarEls.index(this);
 		var navBtnEl = navBarEls.eq(tIndex);
