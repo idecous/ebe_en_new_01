f51172b8a1de7de0642c97ff2b5da44c4f85b8e1
idecous <idecous@gmail.com>
2014-10-18 10:22:49 +0800

    onepage complete

--- public_en/js/customer/checkout/onepage.js
+++ public_en/js/customer/checkout/onepage.js
@@ -77,7 +77,7 @@ EBE_LoginModule.prototype = Object.create(EBE_ModuleBase.prototype);
 				alert( that.leftErrMessage );
 				return;
 			}else{
-				that.nextFn();
+				that.nextFn( that.role );
 			}
 		});
 		this.rightContinueEl.click(function(){
@@ -151,14 +151,15 @@ EBE_BillingModule_Unlogined.prototype = Object.create(EBE_ModuleBase.prototype);
 		});
 		
 		this.continueEl.click(function(){
-			//if( !that.verify() ){ return;}
-			that.nextFn();
+			if( !that.verify() ){ return;}
+			that.nextFn( that.getData(), that.shipRadioEls.eq(0).prop("checked")  ,false );
 		});
 	};
 	this.getData = function(){
 		var data = {};
 		var i,inputEl;
-		for(var i=0; i<this.inputEls.length;i++){
+		var length = this.inputEls.length - (this.role=="Register"?0:2);
+		for(var i=0; i< length;i++){
 			inputEl = this.inputEls.eq(i);
 			data[ inputEl.attr("name") ] = inputEl.val();
 		}
@@ -215,9 +216,85 @@ EBE_BillingModule_Unlogined.prototype = Object.create(EBE_ModuleBase.prototype);
 		this.continueEl = this.el.find(".continueButtton");
 	};
 }).call(EBE_BillingModule_Unlogined.prototype);
-var EBE_BillingModule_Logined = function(editClickFn,patterns){};
-
-
+var EBE_BillingModule_Logined = function(editClickFn,patterns){
+	EBE_ModuleBase.call(this,".billingInformation");
+	this.editBtnClickFn = editClickFn;
+	this.patterns = patterns;
+	this.shipTo = ""; //billing different 
+	this.init();
+};
+EBE_BillingModule_Logined.prototype = Object.create(EBE_ModuleBase.prototype);
+(function(){
+	this.init = function(){
+		this.build();
+		this.superInit();
+		this.updateStatus();
+		var that = this;
+		this.addressSelectorEl.change(function(){
+			that.updateStatus();
+		});		
+		this.continueEl.click(function(){
+			if( that.addressSelectorEl.val() == "" ){
+				if( !that.verify() ){ return;}
+				that.nextFn( that.getData(), that.shipRadioEls.eq(0).prop("checked"),
+				that.saveInputEl.prop("checked") );
+			}else{
+				that.nextFn( that.getData(), that.shipRadioEls.eq(0).prop("checked"),false );
+			}
+		});
+	};
+	this.getData = function(){
+		var selectValue = this.addressSelectorEl.val();
+		if( selectValue == ""){
+			var data = {};
+			var i,inputEl;
+			for( i=0; i< this.inputEls.length;i++){
+				inputEl = this.inputEls.eq(i);
+				data[ inputEl.attr("name") ] = inputEl.val();
+			}
+			return data;
+		}
+		return selectValue;	
+	};
+	this.verify = function(){
+		var i,index,result = true;
+		var inputIndexs = [0,1,3,4,5,6,7,8];
+		for( i=0; i < inputIndexs.length ;i++ ){
+			index = inputIndexs[i];
+			if( $.trim( this.inputEls.eq(index).val()) == "" ){
+				result = false;
+				this.warnEls.eq(index).css("visibility","visible");
+			}else{
+				this.warnEls.eq(index).css("visibility","hidden");
+			}	
+		}
+		return result;
+	};
+	this.updateStatus = function(){
+		if( this.addressSelectorEl.val() == "" ){
+			this.inputUnitEls.show();
+			this.saveEl.show();
+		}else{
+			this.inputUnitEls.hide();
+			this.saveEl.hide();
+		}
+	};	
+	this.build = function(){
+		this.addressSelectorEl = this.el.find("select:eq(0)");
+		this.inputEls = this.el.find("input[type=text],input[type=password],select:gt(0)");
+		this.warnEls = this.el.find(".inputUnit .warn:gt(0)");
+		
+		this.inputUnitEls = this.el.find(".inputUnit:gt(0)");
+		
+		this.saveEl = this.el.find(".saveRow");
+		this.saveInputEl = this.el.find(".saveRow input");
+		this.saveLabelEl = this.el.find(".saveRow span");
+		
+		this.shipRadioEls = this.el.find("input[type=radio]");
+		this.shipRadioLabelEls = this.el.find(".addressRow span");
+		this.continueEl = this.el.find(".continueButtton");
+	};
+}).call(EBE_BillingModule_Logined.prototype);
 
 var EBE_ShippingModule_Unlogined = function(editClickFn,patterns){
 	EBE_ModuleBase.call(this,".shippingInformation");
@@ -243,9 +320,8 @@ EBE_ShippingModule_Unlogined.prototype = Object.create(EBE_ModuleBase.prototype)
 			if( !that.verify() ){return;}
 		});
 		this.continueEl.click(function(){
-			//if( !that.verify() ){ return;}
-			that.nextFn();
-			
+			if( !that.verify() ){ return;}
+			that.nextFn( that.getData(),false );			
 		});
 	};
 	this.copyByBilling = function(){
@@ -257,6 +333,16 @@ EBE_ShippingModule_Unlogined.prototype = Object.create(EBE_ModuleBase.prototype)
 				inputEl.val( data[ inputEl.attr("name") ] );
 			}
 		}
+	};
+	this.getData = function(){
+		var data = {};
+		var i,inputEl;
+		var length = this.inputEls.length;
+		for(var i=0; i< length;i++){
+			inputEl = this.inputEls.eq(i);
+			data[ inputEl.attr("name") ] = inputEl.val();
+		}
+		return data;
 	};
 	this.verify = function(){
 		var i,index,result = true;
@@ -282,67 +368,282 @@ EBE_ShippingModule_Unlogined.prototype = Object.create(EBE_ModuleBase.prototype)
 		this.continueEl = this.el.find(".continueButtton");
 	};
 }).call(EBE_ShippingModule_Unlogined.prototype);
-var EBE_ShippingModule_Logined = function(editClickFn,patterns){};
+var EBE_ShippingModule_Logined = function(editClickFn,patterns){
+	EBE_ModuleBase.call(this,".shippingInformation");
+	this.editBtnClickFn = editClickFn;
+	this.patterns = patterns;
+	this.copyFn = null;
+	this.init();
+};
+EBE_ShippingModule_Logined.prototype = Object.create(EBE_ModuleBase.prototype);
+(function(){
+	this.init = function(){
+		this.build();
+		this.superInit();
+		this.updateStatus();
+		var that = this;
+		this.addressSelectorEl.change(function(){
+			that.updateStatus();
+		});	
+		this.copyInputEl.change(function(){
+			that.copyByBilling();
+		});
+		this.continueEl.click(function(){
+			if( that.addressSelectorEl.val() == "" ){
+				if( !that.verify() ){ return;}
+				that.nextFn( that.getData(),that.saveInputEl.prop("checked") );
+			}else{
+				that.nextFn( that.getData(),false );
+			}
+		});
+	};
+	this.copyByBilling = function(){
+		if( this.copyInputEl.prop("checked") ){
+			var data = this.copyFn();
+			
+			if( $.type(data) === "string" ){
+				this.addressSelectorEl.val(data);
+			}else{
+				this.addressSelectorEl.val("");
+				var i,inputEl;
+				for(i=0; i < this.inputEls.length ;i++ ){
+					inputEl = this.inputEls.eq(i);
+					inputEl.val( data[ inputEl.attr("name") ] );
+				}
+			}
+			this.updateStatus();
+		}
+	};
+	this.updateStatus = function(){
+		if( this.addressSelectorEl.val() == "" ){
+			this.inputUnitEls.show();
+			this.saveEl.show();
+		}else{
+			this.inputUnitEls.hide();
+			this.saveEl.hide();
+		}
+	};
+	this.getData = function(){
+		var selectValue = this.addressSelectorEl.val();
+		if( selectValue == ""){
+			var data = {};
+			var i,inputEl;
+			for( i=0; i< this.inputEls.length;i++){
+				inputEl = this.inputEls.eq(i);
+				data[ inputEl.attr("name") ] = inputEl.val();
+			}
+			return data;
+		}
+		return selectValue;	
+	};
+	this.verify = function(){
+		var i,index,result = true;
+		var inputIndexs = [0,1,3,4,5,6,7,8];
+		for( i=0; i < inputIndexs.length ;i++ ){
+			index = inputIndexs[i];
+			if( $.trim( this.inputEls.eq(index).val()) == "" ){
+				result = false;
+				this.warnEls.eq(index).css("visibility","visible");
+			}else{
+				this.warnEls.eq(index).css("visibility","hidden");
+			}	
+		}
+		return result;
+	};
+	this.build = function(){
+		this.addressSelectorEl = this.el.find("select:eq(0)");
+		this.inputUnitEls = this.el.find(".inputUnit:gt(0)");
+		this.inputEls = this.el.find("input[type=text],select:gt(0)");
+		this.warnEls = this.el.find(".inputUnit .warn:gt(0)");
+		
+		this.saveEl = this.el.find(".operationRow div:eq(0)");
+		this.saveInputEl = this.saveEl.find("input");
+		this.saveLabelEl = this.saveEl.find("span");
+		
+		this.copyEl = this.el.find(".operationRow div:eq(1)");
+		this.copyInputEl = this.copyEl.find("input");
+		this.copyLabelEl = this.copyEl.find("span");
+		
+		this.continueEl = this.el.find(".continueButtton");
+	};
+}).call(EBE_ShippingModule_Logined.prototype);
 
 
-var EBE_ShippingMethodModule = function(){
-	
+var EBE_ShippingMethodModule = function(editClickFn){
+	EBE_ModuleBase.call(this,".shippingMethod");
+	this.editBtnClickFn = editClickFn;
+	this.data = null;
+	this.init();
 };
-var EBE_PaymentModule = function(){
-	
+EBE_ShippingMethodModule.prototype = Object.create(EBE_ModuleBase.prototype);
+(function(){
+	this.init = function(){
+		this.build();
+		this.superInit();
+		var that = this;
+		this.continueEl.click(function(){
+			var value = that.ulEl.find("input:checked").val();
+			that.nextFn( that.data[value] );
+		});
+	};
+	this.setData = function( data ){
+		this.ulEl.empty();
+		this.data = {};
+		var i,itemData,str;
+		for( i=0;i < data.length;i++ ){
+			itemData = data[i];
+			this.data[ itemData.value ] = itemData;
+			$("<li><input type='radio' name='"+itemData.name+"' value='"+itemData.value
+			+"' /><span>"+ itemData.label +"</span></li>").appendTo(this.ulEl);
+		}
+		this.ulEl.find("input:eq(0)").prop("checked",true);
+	};
+	this.build = function(){
+		this.ulEl = this.el.find("ul");
+		this.continueEl = this.el.find(".continueButtton");
+	};	
+}).call(EBE_ShippingMethodModule.prototype);
+
+var EBE_PaymentModule = function(editClickFn){
+	EBE_ModuleBase.call(this,".paymentInformation");
+	this.editBtnClickFn = editClickFn;
+	this.isPop = false;
+	this.init();
 };
-var EBE_ReviewModule = function(){
-	
+EBE_PaymentModule.prototype = Object.create(EBE_ModuleBase.prototype);
+(function(){
+	this.init = function(){
+		this.build();
+		this.superInit();
+		var that = this;
+		this.continueEl.click(function(){
+			if( !that.verify() ){return;}
+			that.nextFn( that.getData() );
+		});
+		this.winEl.mousedown(function(){
+			if( !that.isPop ){return;}
+			that.popWindowEl.hide();
+			that.isPop = false;
+		}).resize(function(){
+			if( !that.isPop ){return;}
+			that.popWindowEl.hide();
+			that.isPop = false;
+		});
+		this.popBtnEl.click(function(e){
+			var offset = that.popBtnEl.offset();
+			var top = offset.top + 20;
+			var left = offset.left + 5;
+			var winWidth = that.winEl.width();
+			that.popWindowEl.show().css({"top":top,"left":left});
+			that.isPop = true;
+		});
+	};
+	this.getData = function(){
+		var data = {};
+		var i,inputEl;
+		for(i=0; i < this.inputEls.length;i++){
+			inputEl = this.inputEls.eq(i);
+			data[ inputEl.attr("name") ] = inputEl.val();
+		}
+		return data;
+	};
+	this.verify = function(){
+		var i,inputVal,result = true;
+		for( i=0; i < this.inputEls.length;i++){
+			inputVal = this.inputEls.eq(i).val();
+			if( $.trim( inputVal ) == "" ){
+				result = false;
+				this.warnEls.eq(i).css("visibility","visible");
+			}else{
+				this.warnEls.eq(i).css("visibility","hidden");
+			}
+		}
+		return result;
+	};
+	this.build = function(){
+		this.bodyEl = $("body");
+		this.winEl = $(window);
+		this.inputEls = this.el.find("input[type=text],select");
+		this.warnEls = this.el.find(".inputUnit .warn");
+		this.continueEl = this.el.find(".continueButtton");
+		
+		this.popBtnEl = this.el.find(".inputUnit .descript");
+		this.popWindowEl = $(".popWindow");
+	};	
+}).call(EBE_PaymentModule.prototype);
+
+var EBE_ReviewModule = function(editClickFn){
+	EBE_ModuleBase.call(this,".orderReview");
+	this.editBtnClickFn = editClickFn;
+	this.init();
 };
+EBE_ReviewModule.prototype = Object.create(EBE_ModuleBase.prototype);
+(function(){
+	this.init = function(){
+		this.build();
+		this.superInit();
+		var that = this;
+		this.continueEl.click(function(){
+			that.nextFn();
+		});
+	};
+	this.setData = function(data){
+		this.listEls.find("li:gt(0)").remove();
+		var i,itemData;
+		for( var i = 0 ; i < data.list.length ;i++ ){
+			itemData = data.list[i];
+			$("<li><div class='paramCol'>h1>"+itemData.name+"</h1><div>"+ itemData.color+"</div>"+
+			"<div>"+itemData.size+"</div><div>"+itemData.price+
+			"</div></div><div class='quantityCol'>"+itemData.QTY+"</div>"+
+			"<div class='price'><b>"+itemData.subtotal+"</b></div></li>").appendTo(this.listEls);
+		}
+		
+		for(i=0; i < data.total.length;i++){
+			itemData = data.total[i];
+			this.totalLabelEls.eq(i).text(  itemData[0]);
+			this.totalPriceEls.eq(i).text(  itemData[1]);
+		}
+	};
+	this.build = function(){
+		this.listEls = this.el.find(".listBlock");
+		this.totalLabelEls = this.el.find(".totalBlock li>.label:empty,.totalBlock li>.label>b ");
+		this.totalPriceEls = this.el.find(".totalBlock li>.priceCol:empty,.totalBlock li>.priceCol>b ");
+		this.continueEl = this.el.find(".continueButtton");
+	};
+}).call(EBE_ReviewModule.prototype);
 
 var EBE_CheckOutManager = function(patterns){
 	var i,modules = [];
 	var currentIndex = 0;
 
 	var loginModule = new EBE_LoginModule(changeModuleByEdit,patterns);
-	var logined = loginModule.enable;
-	
-	var billingModule = logined?new EBE_BillingModule_Unlogined(changeModuleByEdit,patterns):
-								new EBE_BillingModule_Logined(changeModuleByEdit,patterns);
-	modules.push(billingModule);
-	
-	var shippingModule = logined? new EBE_ShippingModule_Unlogined(changeModuleByEdit,patterns):
-								  new EBE_ShippingModule_Logined(changeModuleByEdit,patterns);
-	modules.push(shippingModule);
-	
-	
-	if( loginModule.enable  ){
-		modules.splice(0,0,loginModule);
-		loginModule.nextFn = function(){
-			changeModuleByEdit(1);
-			billingModule.setRole( loginModule.role );
-		};
+	var logined = !loginModule.enable;
+	if( !logined ){
+		modules.push(loginModule);
 	}
+	var billingModule = logined?new EBE_BillingModule_Logined(changeModuleByEdit,patterns):
+								new EBE_BillingModule_Unlogined(changeModuleByEdit,patterns);
+	modules.push(billingModule);
+	var shippingModule = logined? new EBE_ShippingModule_Logined(changeModuleByEdit,patterns):
+								  new EBE_ShippingModule_Unlogined(changeModuleByEdit,patterns);
+	modules.push(shippingModule);
+	var shippingMethodModule = new EBE_ShippingMethodModule(changeModuleByEdit);
+	modules.push(shippingMethodModule);
+	var paymentModule = new EBE_PaymentModule(changeModuleByEdit);
+	modules.push(paymentModule);
+	var reviewModule = new EBE_ReviewModule(changeModuleByEdit);
+	modules.push(reviewModule);
+	
 	for(i=0;i<modules.length;i++){
 		modules[i].index = i;
 	}
-	
-	
-	billingModule.nextFn = function(){
-		if( billingModule.shipTo == "billing" ){
-			changeModuleByEdit( currentIndex + 2);
-		}else{
-			changeModuleByEdit( currentIndex + 1);
-		}
-	};
-	shippingModule.nextFn = function(){
-		
-	};
 	shippingModule.copyFn = function(){
-		return billingModule.getData();
+		return billingModule.getData({"value":"7","name":"FedEx","label":"FedEx Ground $7.00"  });
 	};
-	
-	
-	
-
 
 	function changeModuleByEdit(index){
 		currentIndex = index;
+	
 		for(var i=0; i < modules.length;i++){
 			if( i < currentIndex){
 				modules[i].setAllow();
@@ -353,36 +654,107 @@ var EBE_CheckOutManager = function(patterns){
 			}
 		}
 	};
-	
-	
-	function setLoginHandler(fn){
-		loginModule.loginFn = fn;
-	};
-	function setLoginError(err){
-		loginModule.setLoginError(err);
-	};
-	function setError(err01){
-		loginModule.leftErrMessage = err01;
+	function nextModule(){
+		if( !logined && currentIndex == 0){
+			billingModule.setRole( loginModule.role );
+		}
+		changeModuleByEdit( currentIndex + 1);
 		
 		
 	}
 	
 	
+	function setError(err01){
+		loginModule.leftErrMessage = err01;
+	}
+	function setLoginError(err){
+		loginModule.setLoginError(err);
+	};
+	function setLoginHandler(fn){
+		loginModule.loginFn = fn;
+	};
+	
+	function setUnloginedHandler(fn){
+		loginModule.nextFn = fn;
+	}
+	function setBillingHandler(fn){
+		billingModule.nextFn = fn;
+	}
+	function setShippingHandler(fn){
+		shippingModule.nextFn = fn;
+	}
+	function setShippingMethodHandler(fn){
+		shippingMethodModule.nextFn = fn;
+	}
+	function setPaymentHandler(fn){
+		paymentModule.nextFn = fn;
+	}
+	function setSaveHandler(fn){
+		reviewModule.nextFn = fn;
+	}
+
+	function setShippingMethodData(data){
+		shippingMethodModule.setData(data);
+	}
+	function setReviewData(data){
+		reviewModule.setData(data);
+	}
+	
 	return {
 		"setError":setError,
 		"setLoginError":setLoginError,
-		"setLoginHandler":setLoginHandler
+		"setLoginHandler":setLoginHandler,
+		"setUnloginedHandler":setUnloginedHandler,
+		"setBillingHandler":setBillingHandler,
+		"setShippingHandler":setShippingHandler,
+		"setShippingMethodHandler":setShippingMethodHandler,
+		"setPaymentHandler":setPaymentHandler,
+		"setSaveHandler":setSaveHandler,
+		"nextModule":nextModule,
+		"setShippingMethodData":setShippingMethodData,
+		"setReviewData":setReviewData
 	};
 };
+
+
+
+var countID = 600;
+var shippingMethodData = [
+	{"value":"1","name":"FedEx","label":"FedEx Ground $1.00"  },
+	{"value":"2","name":"FedEx","label":"FedEx Ground $2.00"  },
+	{"value":"3","name":"FedEx","label":"FedEx Ground $3.00"  }
+];
+function getReviewData( size ){
+	var i,arr = [];
+	for( i=0; i < size;i++ ){
+		arr.push({
+			"name":"ABBY DEEP V TOP__" + countID,
+			"color":"Color BLACK",
+			"size":"Size S",
+			"price":"$1,333.00",
+			"QTY":"999",
+			"subtotal":"$888,333.00"
+		});
+		countID++;
+	}
+	var totalData = [
+		["Subtotal","$5,919.00 "],
+		["Shipping & Handling (Select Shipping Method - FedEx 2-Day)","$9.00"],
+		["Grand Total","$939.00"]
+	];
+	return {"list":arr,"total":totalData};
+}
+
 
 
 $(function(){
 	var checkOutManager = new EBE_CheckOutManager({
 		"email":/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
-		"password": /^[a-zA-Z0-9!@#$%^&*]{6,16}$/i
+		"password": /^[a-zA-Z0-9!@#$%^&*]{1,16}$/i
 	});
 	
 	checkOutManager.setError("请选择地区或以来宾身份结账!");
+	
 	
 	checkOutManager.setLoginHandler(function(name,password){
 		console.log("登陆(用户名/密码)",name,password);
@@ -392,11 +764,53 @@ $(function(){
 		//失败
 		checkOutManager.setLoginError("无效的登录名或密码。");
 	});
-	
-	
-	
-	
-	
+	checkOutManager.setUnloginedHandler(function(role){
+		console.log( "未登录访问方式", role);
+		//
+		checkOutManager.nextModule();		
+	});
+	checkOutManager.setBillingHandler(function(data,sameddress,isSave){
+		console.log( "账单信息(数据/是否送货到同一地方/是否保存)", data,sameddress,isSave);
+		//请求服务器
+		/*
+		 * 根据 $.type(data) === "string" 来判断是否为 地址ID
+		 */
+		if( sameddress ){
+			checkOutManager.setShippingMethodData(shippingMethodData);
+			checkOutManager.nextModule();		
+			checkOutManager.nextModule();		
+		}else{
+			checkOutManager.nextModule();		
+		}
+	});
+	checkOutManager.setShippingHandler(function(data,isSave){
+		console.log("送货地址信息(数据/是否保存)", data,isSave);
+		//请求服务器
+		/*
+		 * 根据 $.type(data) === "string" 来判断是否为 地址ID
+		 */
+		checkOutManager.setShippingMethodData(shippingMethodData);
+		checkOutManager.nextModule();
+	});
+	checkOutManager.setShippingMethodHandler(function(value){
+		console.log("送货方式",value);
+		if( value == undefined ){
+			alert("请选择送货方式");
+			return;
+		}
+		//
+		checkOutManager.nextModule();
+	});
+	checkOutManager.setPaymentHandler(function(data){
+		console.log("支付方式",data);
+		//
+		checkOutManager.setReviewData( getReviewData(2) );
+		checkOutManager.nextModule();		
+	});
+	checkOutManager.setSaveHandler(function(){
+		console.log("流程完成");
+		//
+	});
 	
 	
 	
